6411285ca078b546a5aafe65bf04c28854671422
lamvietthanh61-lab <lamvietthanh61@gmail.com>
2025-12-14 23:18:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function searchPosts() {
+  let key = document.getElementById("searchInput").value.toLowerCase();
+  let posts = document.getElementsByClassName("post");
+
+  for (let p of posts) {
+    let text = p.innerText.toLowerCase();
+    p.style.display = text.includes(key) ? "" : "none";
+  }
+}
+
+function toggleMode() {
+  document.body.classList.toggle("dark");
+}
 

4504318dbfdbaf0f2b57485471bda92e1a37a3b3
lamvietthanh61-lab <lamvietthanh61@gmail.com>
2025-12-15 10:43:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,4 +11,19 @@ function searchPosts() {
 function toggleMode() {
   document.body.classList.toggle("dark");
 }
+// THÔNG TIN ADMIN (bạn tự đổi)
+const ADMIN_USER = "adminthanhdz";
+const ADMIN_PASS = "Adminthanh@123"; // ≥8 ký tự, chữ hoa, số, ký tự đặc biệt
+
+function login() {
+  let user = document.getElementById("adminUser").value;
+  let pass = document.getElementById("adminPass").value;
+
+  if (user === ADMIN_USER && pass === ADMIN_PASS) {
+    localStorage.setItem("adminLogin", "true");
+    window.location.href = "admin.html";
+  } else {
+    document.getElementById("error").innerText = "❌ Sai tài khoản hoặc mật khẩu";
+  }
+}
 
